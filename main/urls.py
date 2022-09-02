a0bc2dbcd40f3e5803e5b3aa37995590b01d9d1b
Kirabo Ibrahim <kiraboibra268@gmail.com>
2022-09-02 17:02:35 +0300

    refactor: Add names to the url definitions

--- main/urls.py
+++ main/urls.py
@@ -5,9 +5,9 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("profile/", views.profile_page, name="profile"),
     path("courses/", views.courses, name="courses"),
-    path("courses/<int:id>/", views.course_page),
-    path("courses/<int:id_course>/<int:id_lesson>/", views.lesson_page),
+    path("courses/<int:id>/", views.course_page, name="course_detail"),
+    path("courses/<int:id_course>/<int:id_lesson>/", views.lesson_page, name="course_lesson"),
     # path("lessons/<int:id>/", views.lesson_page),
-    path("admin_pannel/", views.admin_page),
-    path("courses/<int:id_course>/<int:id_lesson>/action/", views.action),
+    path("admin_pannel/", views.admin_page, name="admin_panel"),
+    path("courses/<int:id_course>/<int:id_lesson>/action/", views.action, name="lesson_completed"),
     ]
