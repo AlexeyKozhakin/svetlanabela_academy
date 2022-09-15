5af5a99f0b57712ec281816222b05f11b24a313d
Kirabo Ibrahim <kiraboibra268@gmail.com>
2022-09-15 09:35:21 +0300

    fix: Remove DoesNotExist exception for users that dont have a profile

--- main/context_processors.py
+++ main/context_processors.py
@@ -3,8 +3,10 @@ from main.models import Profile
 
 def user_profile(request):
     user = request.user
-
+    profile_context = {"profile": None}  # Users with no profile ie superuser
     if user.is_authenticated:
-        return {"profile": Profile.objects.get(user=user)}
-    else:
-        return {"profile": None}
+        try:
+            profile_context["profile"] = Profile.objects.get(user=user)
+        except Profile.DoesNotExist:
+            pass
+    return profile_context
